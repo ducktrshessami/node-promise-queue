b9aa43cee19e14ad8a0ae71362619c95da0d8e82
ducktrshessami <ducktrshessami@users.noreply.github.com>
2024-11-02 23:16:30 +0000

    Build 60b3b0fe62366b48b7bb427a718f481bc5f7c9a7

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,30 +1,23 @@
 type LazyPromise<T> = () => Promise<T>;
 type PromiseOrLazy<T> = Promise<T> | LazyPromise<T>;
-type MergeGroupType<Groups extends {}, Group extends string, T> = Group extends keyof Groups ? (Groups[Group] extends any[] ? [...Groups[Group], T] : []) : [
-    T
-];
-type MergeGroup<Groups extends {}, Group extends string, T> = Omit<Groups, Group> & {
-    [K in Group]: MergeGroupType<Groups, K, T>;
-};
 
-declare class PromiseQueueGroup<GroupTypes extends any[] = []> {
-    private readonly promises;
+declare class PromiseQueueGroup {
+    readonly promises: Promise<any>[];
     private _all;
     constructor();
-    get all(): Promise<GroupTypes>;
-    protected push<T>(promise: Promise<T>): asserts this is PromiseQueueGroup<[...GroupTypes, T]>;
+    get all(): Promise<any[]>;
+    protected push(promise: Promise<any>): void;
     resolveLazy<T>(promise: PromiseOrLazy<T>): Promise<T>;
-    add<T>(promise: PromiseOrLazy<T>): asserts this is PromiseQueueGroup<[...GroupTypes, T]>;
-    clear(): asserts this is PromiseQueueGroup<[]>;
+    add(promise: PromiseOrLazy<any>): void;
+    clear(): void;
 }
 
-declare class PromiseQueue<GroupMap extends {} = {}, AllTypes extends any[] = []> extends PromiseQueueGroup<AllTypes> {
+declare class PromiseQueue extends PromiseQueueGroup {
     static readonly DefaultGroup = "default";
     private readonly groups;
     private ensureGroup;
-    add<T>(promise: PromiseOrLazy<T>): asserts this is PromiseQueue<MergeGroup<GroupMap, typeof PromiseQueue.DefaultGroup, T>, [...AllTypes, T]>;
-    add<T, Group extends string>(promise: PromiseOrLazy<T>, groupName: Group): asserts this is PromiseQueue<MergeGroup<GroupMap, Group, T>, [...AllTypes, T]>;
-    clear(): asserts this is PromiseQueue<{}, []>;
+    add(promise: PromiseOrLazy<any>, groupName?: string): void;
+    clear(): void;
 }
 
 export { type LazyPromise, type PromiseOrLazy, PromiseQueue, PromiseQueueGroup };
