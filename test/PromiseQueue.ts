edcc25985f148e1a0f1f5191c1cf2cea2a8e85cb
ducktrshessami <cal.tf2.3@gmail.com>
2024-11-02 16:59:52 -0700

    chore: test lazy promise queueing

--- test/PromiseQueue.ts
+++ test/PromiseQueue.ts
@@ -9,4 +9,17 @@ describe("PromiseQueue", function () {
         const results = await queue.all;
         assert.deepStrictEqual(results, [1, 2]);
     });
+
+    it("should resolve lazy Promises after stored Promises", async function () {
+        const queue: PromiseQueue = new PromiseQueue();
+        let resolve: () => void;
+        queue.add(new Promise(res => resolve = () => {
+            const t = Date.now();
+            setTimeout(() => res(t), 1);
+        }));
+        queue.add(() => Promise.resolve(Date.now()));
+        resolve!();
+        const results = await queue.all;
+        assert(results[0] < results[1]);
+    });
 });
