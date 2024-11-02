8ddca453771a5312ae4047c179b18f60c8c8e426
ducktrshessami <cal.tf2.3@gmail.com>
2024-11-02 16:24:04 -0700

    refactor: genericless test

--- test/PromiseQueueGroup.ts
+++ test/PromiseQueueGroup.ts
@@ -3,7 +3,7 @@ import { PromiseQueueGroup } from "../";
 
 describe("PromiseQueueGroup", function () {
     it("should contain stored Promises", async function () {
-        const group: PromiseQueueGroup<[]> = new PromiseQueueGroup();
+        const group: PromiseQueueGroup = new PromiseQueueGroup();
         group.add(Promise.resolve(1));
         group.add(Promise.resolve(2));
         const results = await group.all;
