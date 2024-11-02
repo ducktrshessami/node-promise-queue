14948f03f7f0641166cb93143146eafae735b556
ducktrshessami <cal.tf2.3@gmail.com>
2024-11-02 16:02:45 -0700

    chore: add test

---
+++ test/PromiseQueueGroup.ts
@@ -0,0 +1,12 @@
+import assert from "assert";
+import { PromiseQueueGroup } from "../";
+
+describe("PromiseQueueGroup", function () {
+    it("should contain stored Promises", async function () {
+        const group: PromiseQueueGroup<[]> = new PromiseQueueGroup();
+        group.add(Promise.resolve(1));
+        group.add(Promise.resolve(2));
+        const results = await group.all;
+        assert.deepStrictEqual(results, [1, 2]);
+    });
+});
